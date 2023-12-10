6aa5e79b7d9b25214ec66dd8e7be70c7f24970fe
Gabriel Medero <gabrielmedcap@hotmail.com>
2023-12-10 18:34:59 -0300

    Ajustes homme a mobile

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 const nav = document.querySelector("#nav");
+const casos = document.querySelector("#casos");
 const abrir = document.querySelector("#btn-menu");
 const cerrar = document.querySelector("#btn-cerrar");
 
@@ -8,4 +9,12 @@ abrir.addEventListener("click", () => {
 
 cerrar.addEventListener("click", () => {
     nav.classList.remove("visible");
+})
+
+abrir.addEventListener("click", () => {
+    casos.classList.toggle("visible");
+})
+
+cerrar.addEventListener("click", () => {
+    casos.classList.remove("visible");
 })
